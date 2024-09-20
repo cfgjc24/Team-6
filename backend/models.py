8e32d9f1162923a0c01759cad2dc63cf0b5d7e02
Samuel Buena <sbuena@cs.stonybrook.edu>
2024-09-20 19:57:21 -0400

    push model for Lessons

--- backend/models.py
+++ backend/models.py
@@ -1,2 +1,9 @@
 from app import db
-
+class Lesson(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    completed = db.Column(db.Boolean, unique=False, nullable=False)
+    question_responses = db.arrays(db.String, unique=False)
+    confidence_level = db.Column(db.Integer, unique=False, nullable=False)
+    belonging_level = db.Column(db.Integer, unique=False, nullable=False)
+    biggest_challenge = db.Column(db.String, unique=False, nullable=False)
+    suggestions = db.Column(db.String, unique=False, nullable=False)
