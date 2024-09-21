b6adf87e27e1d44b46185cc5ab7ee516cb97542e
elaineran <eyr4@cornell.edu>
2024-09-21 00:49:43 -0400

    added admin methods, like getting confidence levels and belonging levels

--- backend/app.py
+++ backend/app.py
@@ -31,6 +31,7 @@ def login():
 def register():
     return jsonify({"Successfully registered!"})
 
+# Get average confidence levels for each lesson
 @app.route("/api/admin/confidence", methods=["GET"])
 def get_confidence_levels():
     confidence_levels = defaultdict(list)
@@ -42,6 +43,27 @@ def get_confidence_levels():
     for lesson in confidence_levels:
         confidence_level_per_lesson[lesson.id] = sum(confidence_levels[lesson])/len(confidence_levels[lesson])
     return jsonify(confidence_level_per_lesson)
+
+# Get confidence level for a specific lesson
+@app.route("/api/admin/confidence/<lesson_id>", methods=["GET"])
+def get_lesson_confidence_level(lesson_id):
+    all_students = Student.query.all()  
+    for student in all_students:
+        confidence_levels = [lesson.confidence_level for lesson in student.lessons if lesson.id == lesson_id]
+    return jsonify(confidence_levels)
+
+# Get average belonging levels for each lesson
+@app.route("/api/admin/belonging", methods=["GET"])
+def get_confidence_levels():
+    belonging_levels = defaultdict(list)
+    belonging_level_per_lesson = defaultdict(int)
+    all_students = Student.query.all()  
+    for student in all_students:
+        for lesson in student.lessons:
+            belonging_levels[lesson.id].append(lesson.belonging_level)
+    for lesson in belonging_levels:
+        belonging_level_per_lesson[lesson.id] = sum(belonging_levels[lesson])/len(belonging_levels[lesson])
+    return jsonify(belonging_level_per_lesson)
 
 
 # Get all students
