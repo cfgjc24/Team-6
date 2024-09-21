4adc2195e34d3248784ab32aee66028d4ebec6e3
Angelina <158765888+aangelinali@users.noreply.github.com>
2024-09-21 05:35:25 -0400

    Updated functionalities to load students.json into the database by taking into account model association tables

--- backend/bootstrap.py
+++ backend/bootstrap.py
@@ -24,14 +24,22 @@ def create_student():
 def load_data():
     with open('students.json') as file:
         data = json.load(file)
-        for student in data:
-            user = Student(id=student['id'], first_name=student['first_name'], last_name=student['last_name'],
-                email=student['email'], school=student['school'], password=student['password'], 
-                tutor=student['tutor'], all_lessons=student['all_lessons'], 
-                lessons_completed=student['lessons_completed'])
-            db_.session.add(user)
+        lessons = {}
+        for item in data:
+            for student_lesson in item['lessons']:
+                if student_lesson not in lessons:
+                    student = Student(id=student_lesson)
+                    db_.session.add(student)
+                    lessons[student_lesson] = student
+            
+            student = Student(id=item['id'], first_name=item['first_name'], 
+                              last_name=item['last_name'],)
+            for student_lesson in item['lessons']:
+                lesson = lessons[student_lesson]
+                student.lessons.append(lessons[student_lesson])
+            db_.session.commit()
         db_.session.commit()
-        print("Data loaded.")
+        print("Students and lessons created.")
 
 if __name__ == "__main__":
     # Delete existing database before bootstrapping a new one
