03a48d1442a44db8d22cf09fb23f9906e46955a5
elaineran <eyr4@cornell.edu>
2024-09-20 22:27:36 -0400

    added association table for students and lessons

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,13 @@
 from app import db
 from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.schema import ForeignKey
 
+# Association Table linking Student and Lesson
+student_lesson_association = db.Table('student_lesson',
+    db.Column('student_id', db.Integer, db.ForeignKey('student.id'), primary_key=True),
+    db.Column('lesson_id', db.Integer, db.ForeignKey('lesson.id'), primary_key=True)
+)
 
 class Lesson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,7 +19,7 @@ class Lesson(db.Model):
     biggest_challenge = db.Column(db.String(120), unique=False, nullable=False)
     suggestions = db.Column(db.String(120), unique=False, nullable=False)
     
-    student = relationship("Student", back_populates="lessons")
+    students = relationship('Student', secondary=student_lesson_association, back_populates='lessons')
 
 # Define student model for database
 class Student(db.Model):
@@ -28,7 +33,8 @@ class Student(db.Model):
    all_lessons = db.Column(ARRAY(db.String(150)), nullable=False)
    lessons_completed = db.Column(ARRAY(db.String(50)), nullable=False)
 
-   lessons = relationship("Lesson", back_populates="student", cascade="all, delete-orphan")
+   lessons = relationship('Lesson', secondary=student_lesson_association, back_populates='students')
+
    tutor = relationship("Tutor", back_populates="student")
 
 # Define tutor model for database
